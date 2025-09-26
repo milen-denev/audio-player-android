431bfe15afc38c838fe1ddbd7d0a15d909510376
Milen Denev <mspc6d25@hotmail.com>
2025-09-26 18:43:37 +0300

    remove logging.

--- examples/android_main.rs
+++ examples/android_main.rs
@@ -5,14 +5,14 @@
 pub fn main() {
     #[cfg(target_os = "android")]
     {
-        // Initialize logging to logcat
-        android_logger::init_once(
-            android_logger::Config::default()
-                .with_max_level(log::Level::Error)
-                .with_tag("rust-audio-player"),
-        );
-        log::info!("Android main() started");
-        // Prefer software renderer to avoid GL issues that can cause a black screen
+        // // Initialize logging to logcat
+        // android_logger::init_once(
+        //     android_logger::Config::default()
+        //         .with_max_level(log::Level::Error)
+        //         .with_tag("rust-audio-player"),
+        // );
+        // log::info!("Android main() started");
+        // // Prefer software renderer to avoid GL issues that can cause a black screen
         unsafe { std::env::set_var("SLINT_RENDERER", "software") };
     }
 
